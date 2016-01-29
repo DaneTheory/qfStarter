5a4d92c2886fcf831c4dced235c834350fca8db5
Branden Dane <branden@danetheory.com>
2016-01-29 03:59:51 -0500

    QuckFrame BG
    Created SVG. Added fill effect (looping GIF currently). Basic start of
    countdown timer.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,7 +2,19 @@
 
   'use strict';
 
-  console.log("JS ready");
+  var gifs = [
+                  "http://i.giphy.com/mI3J3e2v97T8Y.gif",
+                  "http://i.giphy.com/yniHLH1gAigX6.gif",
+                  "http://i.giphy.com/dRGWII2nDN1D2.gif",
+                  "http://i.giphy.com/13ln9K5TWkNTLa.gif",
+                  "http://i.giphy.com/d2ZeRHNZFn8EQ2EE.gif",
+                  "http://i.giphy.com/28JsMYKbeuBGM.gif"
+            ];
+
+  	var randoNum = Math.floor((Math.random() * gifs.length)),
+        qfSvgEl = document.querySelector("#qfImg"),
+        randoGif = qfSvgEl.setAttribute("xlink:href", gifs[randoNum]);
+  
 
   /*
   * For jQuery Uncomment Below
