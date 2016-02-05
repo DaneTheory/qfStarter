82f43e039cbf5daa733613dcc4c48f8fe3e1b27b
Branden Dane <branden@danetheory.com>
2016-02-05 10:23:49 -0500

    blacks change
    removed blocks. Different timer setup.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -19,51 +19,134 @@
     /*===
       COUNTDOWN
     =====*/
-    var competitionsDeadline = 'Febuary 5 2016';
-    var requestsDeadline = 'Febuary 7 2016';
+    // var competitionsDeadline = 'Febuary 5 2016';
+    // var requestsDeadline = 'Febuary 7 2016';
+    //
+    // function getTimeRemaining(endtime) {
+    //   var t = Date.parse(endtime) - Date.parse(new Date()),
+    //   seconds = Math.floor((t / 1000) % 60),
+    //   minutes = Math.floor((t / 1000 / 60) % 60),
+    //   hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+    //   days = Math.floor(t / (1000 * 60 * 60 * 24));
+    //     return {
+    //       'total': t,
+    //       'days': days,
+    //       'hours': hours,
+    //       'minutes': minutes,
+    //       'seconds': seconds
+    //     };
+    //   }
+    //
+    //   function initializeClock(id, endtime) {
+    //     var clock = document.getElementById(id),
+    //     daysSpan = clock.querySelector('.days'),
+    //     hoursSpan = clock.querySelector('.hours'),
+    //     minutesSpan = clock.querySelector('.minutes'),
+    //     secondsSpan = clock.querySelector('.seconds');
+    //
+    //   function updateClock() {
+    //     var t = getTimeRemaining(endtime);
+    //
+    //       daysSpan.textContent = t.days;
+    //       hoursSpan.textContent = ('0' + t.hours).slice(-2);
+    //       minutesSpan.textContent = ('0' + t.minutes).slice(-2);
+    //       secondsSpan.textContent = ('0' + t.seconds).slice(-2);
+    //
+    //         if (t.total <= 0) {
+    //           clearInterval(timeinterval);
+    //         }
+    //   }
+    //
+    //   updateClock();
+    //     var timeinterval = setInterval(updateClock, 1000);
+    //   }
+    //
+    //   initializeClock('competitionsDemo', competitionsDeadline);
+    //   initializeClock('requestsDemo', requestsDeadline);
 
-    function getTimeRemaining(endtime) {
-      var t = Date.parse(endtime) - Date.parse(new Date()),
-      seconds = Math.floor((t / 1000) % 60),
-      minutes = Math.floor((t / 1000 / 60) % 60),
-      hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-      days = Math.floor(t / (1000 * 60 * 60 * 24));
-        return {
-          'total': t,
-          'days': days,
-          'hours': hours,
-          'minutes': minutes,
-          'seconds': seconds
-        };
-      }
 
-      function initializeClock(id, endtime) {
-        var clock = document.getElementById(id),
-        daysSpan = clock.querySelector('.days'),
-        hoursSpan = clock.querySelector('.hours'),
-        minutesSpan = clock.querySelector('.minutes'),
-        secondsSpan = clock.querySelector('.seconds');
 
-      function updateClock() {
-        var t = getTimeRemaining(endtime);
+/*====
+  Countdown 2
+===========*/
+    $(function() {
 
-          daysSpan.textContent = t.days;
-          hoursSpan.textContent = ('0' + t.hours).slice(-2);
-          minutesSpan.textContent = ('0' + t.minutes).slice(-2);
-          secondsSpan.textContent = ('0' + t.seconds).slice(-2);
+      var Countdown = function(options) {
+        $.extend(this, {
+          endDate: new Date(2016, 4, 26, 15, 02, 20, 0)
+        }, options);
 
-            if (t.total <= 0) {
-              clearInterval(timeinterval);
+        this.cache();
+        this.bind();
+
+        return this;
+      };
+      $.extend(Countdown.prototype, {
+        cache: function() {
+          this.date = new Date();
+          this.secCounter = parseInt((this.endDate - this.date) / 1000);
+        },
+        bind: function() {
+          this.timer();
+        },
+        timer: function() {
+          var timeInSec = this.secCounter,
+            $secondsCircle = $('.seconds').closest('.time-circle').find('.progress'),
+            $minutesCircle = $('.minutes').closest('.time-circle').find('.progress'),
+            $hoursCircle = $('.hours').closest('.time-circle').find('.progress'),
+            $seconds = $('.seconds'),
+            $minutes = $('.minutes'),
+            $hours = $('.hours');
+
+          function pad(number) {
+            return (number < 10 ? '0' : '') + number
+          }
+
+          function setScale(type, degree) {
+            type.css({
+              '-webkit-transform': 'rotate(' + -degree * 6 + 'deg)',
+              '-moz-transform': 'rotate(' + -degree * 6 + 'deg)',
+              '-ms-transform': 'rotate(' + -degree * 6 + 'deg)',
+              '-o-transform': 'rotate(' + -degree * 6 + 'deg)',
+              'transform': 'rotate(' + -degree * 6 + 'deg)'
+            });
+          };
+
+          var setInterv = setInterval(function() {
+
+            (timeInSec > 0 ? timeInSec-- : timeInSec = 0);
+
+            var getSeconds = timeInSec % 60,
+              getMinutes = Math.floor(timeInSec / 60 % 60),
+              getHours = Math.floor(timeInSec / 3600 % 24),
+              getDays = Math.floor(timeInSec / 86400 % 7),
+              getWeeks = Math.floor(timeInSec / 604800);
+
+            $seconds.text(pad(getSeconds));
+            $minutes.text(pad(getMinutes));
+            $hours.text(pad(getHours));
+
+            setScale($secondsCircle, getSeconds);
+            setScale($minutesCircle, getMinutes);
+            setScale($hoursCircle, getHours);
+
+            if (timeInSec <= 0) {
+              clearInterval(setInterv);
+              console.log('End of counting');
             }
-      }
 
-      updateClock();
-        var timeinterval = setInterval(updateClock, 1000);
-      }
+          }, 1000);
+        }
+      });
+      window.Countdown = Countdown;
+    });
 
-      initializeClock('competitionsDemo', competitionsDeadline);
-      initializeClock('requestsDemo', requestsDeadline);
-
+    $(function() {
+      var app = new Countdown({
+        //properties
+        //endDate: new Date(year, month, day, hour, minute, second, miliseco)
+      });
+    });
 
 })(jQuery, window, document);
 
